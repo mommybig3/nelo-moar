1783ff6f356ec81511d13c2d2c5b54bd8e8a4677
mommybig3 <42816129+mommybig3@users.noreply.github.com>
2018-08-29 14:46:05 -0400

    Update ModManifest.js

--- ModManifest.js
+++ ModManifest.js
@@ -9,14 +9,7 @@ sheets:{'imageSheet':'https://i.imgur.com/mZaBqH2.png'}
 func:function()
 	{	
 		//resources 
-	new G.Res({
-		name:'Fruit Juice',
-		desc:'[Fruit Juice] tastes better than [water].',
-		icon:[0,0,'imageSheet'],
-		turnToByContext:{'eat':{'health':0.06,'happiness':0.1},'decay':{'spoiled food':0.2}},//this basically translates to : "when eaten, generate some health and happiness; when rotting, turn into either nothing or some spoiled food"
-		partOf:'food',
-		category:'food',
-	});
+	//blank
 		//gathery stuffy codey
 	G.getDict('digger').res['gather']['Bugs']=0.03;
 }
