9fe4673e164b36cd1add6de1440dbc6ea690efb1
mommybig3 <42816129+mommybig3@users.noreply.github.com>
2018-08-29 15:06:00 -0400

    Update ModManifest.js

--- ModManifest.js
+++ ModManifest.js
@@ -5,7 +5,7 @@ desc:'Adds lotz of stuffs!',
 engineVersion:1,
 manifest:'ModManifest.js',
 requires:['Default dataset*'],
-sheets:{'imageSheet':'https://i.imgur.com/mZaBqH2.png'}
+sheets:{'imageSheet':'https://i.imgur.com/mZaBqH2.png'},
 func:function()
 	{	
 		//resources 
