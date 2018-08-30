c2711f6338840589926edd5a204bb2867d3303dc
mommybig3 <42816129+mommybig3@users.noreply.github.com>
2018-08-29 22:11:50 -0400

    Update Manifest.js

--- Manifest.js
+++ Manifest.js
@@ -1,1 +1,20 @@
-
+G.AddData({
+name:'The Moar Mod',
+author:'Mommybig3',
+desc:'Adds lotz of stuffs!',
+engineVersion:1,
+manifest:'Manifest.js',
+requires:['Default dataset*'],
+sheets:{'imageSheet':'https://i.imgur.com/32VPQ1t.png'},
+func:function()
+	{
+		new G.Res({
+			name:'insight',
+			desc:'[magic] requires wisdom to be used, can be a powerful tool.//'+limitDesc('[wisdom]')+'',
+			icon:[0,0],
+			category:'main',
+			limit:'wisdom',
+			getDisplayAmount:researchGetDisplayAmount,
+			whenGathered:researchWhenGathered,
+	});
+});
